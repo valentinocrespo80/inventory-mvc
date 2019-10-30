9eff1759ed1ecb335d800d9f6bc38fc3bcc37323
Valentino Crespo <valentinocrespo80@gmail.com>
2019-10-29 21:17:26 -0500

    initial commit

---
+++ src/main/java/org/launchcode/inventorymvc/InventoryMvcApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.inventorymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/inventorymvc/InventoryMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.inventorymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
